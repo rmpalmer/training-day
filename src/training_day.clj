d474a18f349eeee9f2b3e087f8c0f2409eefc3d1
Richard M. Palmer <richard.m.palmer@gmail.com>
2018-01-16 17:00:52 -0600

    all tests pass

--- src/training_day.clj
+++ src/training_day.clj
@@ -5,7 +5,9 @@
 (def hai "O HAI!")
 
 (defn square [x]
-  ":(")
+  "returns the square of a number"
+  (* x x))
 
 (defn average [a b]
-  ":(")
+  "returns the average of two numbers"
+  (/ (+ a b) 2))
